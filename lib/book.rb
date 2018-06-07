4513dbf58497374a7d95441be5d0411aa14a38ca
hibbahagh <hibbahagh@gmail.com>
2018-06-07 22:12:44 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
 # book.rb
 # Add you Book class here
+
+
+class Book 
+  
+  def initialize(title, author, number_of_page)
+    
+    @title = title
+    @author = author
+    @number_of_page = number_of_page
+    
+    
+    def title
+      @title
+    end
+    
+    def author
+      @author
+    end
+    
+    def number_of_page
+      @number_of_page
+    end
+    
+    book_1 = Book.new()
+    book_2 = 

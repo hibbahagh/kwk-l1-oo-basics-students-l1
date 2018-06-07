84a0caac662643b2a43bc9b2a95ca68eee0de992
hibbahagh <hibbahagh@gmail.com>
2018-06-07 22:18:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,27 +2,27 @@
 # Add you Book class here
 
 
-class Book 
+# class Book 
   
-  def initialize(title, author, number_of_page)
+#   def initialize(title, author, number_of_page)
     
-    @title = title
-    @author = author
-    @number_of_page = number_of_page
+#     @title = title
+#     @author = author
+#     @number_of_page = number_of_page
     
     
-    def title
-      @title
-    end
+#     def title
+#       @title
+#     end
     
-    def author
-      @author
-    end
+#     def author
+#       @author
+#     end
     
-    def number_of_page
-      @number_of_page
-    end
+#     def number_of_page
+#       @number_of_page
+#     end
     
-    book_1 = Book.new("Murder on the Orient Express", Agatha Christie, 256)
+#     book_1 = Book.new("Murder on the Orient Express", Agatha Christie, 256)
     
-    puts book_1.author
+#     puts book_1.author

93456cac23c782b92ef59178f97c37e2a9434e7b
hibbahagh <hibbahagh@gmail.com>
2018-06-07 22:30:51 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -32,12 +32,40 @@
 class Book
   
   def initialize(title)
-    
+    @title = title
   end 
   
   def title
+    @title
   end 
   
+  def author=(author)
+    @author = author 
+  end 
+  
+  def author
+    @author
+  end 
+  
+  def page_count=(num)
+    @page_count = num 
+  end 
+  
+  
+  def page_count 
+    @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def turn_page 
+  puts " fipping the page"
+  end 
+  
+  book = Book.new("Murder on the Orient Express")
+  book.turn_page 
   
   
   
